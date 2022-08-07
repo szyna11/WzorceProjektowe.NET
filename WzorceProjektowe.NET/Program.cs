81c90a89403c8a7621e68f2d748b669df06c66e0
ITEGER Sebastian <sebastian.szymanowicz@iteger.pl>
2022-08-07 14:37:15 +0200

    init

---
+++ WzorceProjektowe.NET/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace WzorceProjektowe.NET
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.ReadLine();
+        }
+    }
+}
