6b8c97da8e82894ba75baa0679d193894dc86f93
ITEGER Sebastian <sebastian.szymanowicz@iteger.pl>
2022-08-07 15:33:43 +0200

    git add

---
+++ WzorceProjektowe.NET/Person.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WzorceProjektowe.NET
+{
+    class Person
+    {
+        //Pola
+        public string Name;
+
+
+        public Person( string Name)
+        {
+            this.Name = Name;
+        }
+
+
+        public void SayHi()
+        {
+            Console.WriteLine("Cześć, nazwym się: " + this.Name);
+        }
+
+    }
+}
---
+++ WzorceProjektowe.NET/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace WzorceProjektowe.NET
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person = new Person("Jan");
+
+            person.SayHi();
+            //Console.WriteLine(person.Name);
+
+            Console.ReadLine();
+        }
+    }
+}
